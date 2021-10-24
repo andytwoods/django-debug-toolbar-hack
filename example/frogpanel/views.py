99d28a596d89844b0bbe2eba4cd34ef391322ab6
andytwoods <andytwoods@gmail.com>
2021-10-24 10:52:12 +0100

    tests

--- example/frogpanel/views.py
+++ example/frogpanel/views.py
@@ -8,21 +8,27 @@ from django.template.loader import get_template
 from django.urls import resolve
 
 
-def open_template(request):
+def _retrieve_template_name(request):
     template_name = request.GET.get('template')
     template = get_template(template_name)
-    return open_element(template.origin.name)
+    return template.origin.name
 
+def open_template(request):
+    template_name = _retrieve_template_name(request)
+    return open_element(template_name)
 
-def open_view(request):
+def _retrieve_view_name(request):
     host = request.get_host()
     referrer = request.META['HTTP_REFERER']
     url_ending = referrer.split(host)[1]
-
     match = resolve(url_ending)
     func, _, _ = match
-    info = inspect.getsourcefile(func)
-    return open_element(info)
+    return inspect.getsourcefile(func)
+
+
+def open_view(request):
+    view_name = _retrieve_view_name(request)
+    return open_element(view_name)
 
 
 def open_element(el):
